91f516c31977ae652f21062a96134b1d34d9e27e
Zhiyuan Huang <Huangzhiyuan999@gmail.com>
2016-10-13 09:47:49 -0600

    Print out sample

--- InheritQuizzer/src/Quizzer.java
+++ InheritQuizzer/src/Quizzer.java
@@ -9,21 +9,53 @@ public class Quizzer
 			int point = 0;
 			Generate.generate();
 			Object option[] = {"The star lives.", "The star dies.", "The dwarf star lives.", "The dwarf star dies.", "ERROR"};
+			System.out.println(
+			"public class Star\n"
+			+"	{\n"
+			+"		public Star()\n"
+			+"		{\n"
+			+"		}\n"
+			+"		public void starLive()\n"
+			+"		{\n"
+			+"			System.out.println(\"The star lives.\");\n"
+			+"		}\n"
+			+"		public void starDie()\n"
+			+"		{\n"
+			+"			System.out.println(\"The star dies.\");\n"
+			+"		}\n"
+			+"	}\n");
+			System.out.println(
+			"public class Dwarfstar extends Star\n"
+			+"	{\n"
+			+"		public Dwarfstar()\n"
+			+"		{\n"
+			+"		}\n"
+			+"		public void dwaftLive()\n"
+			+"		{\n"
+			+"			System.out.println(\"The dwarf star lives.\");\n"
+			+"		}\n"
+			+"		public void starDie()\n"
+			+"		{\n"
+			+"			System.out.println(\"The dwarf star dies.\");\n"
+			+"		}\n"
+			+"	}\n");
 			System.out.println("Input number of questions you want.(From 1 to 12)");
 			int times = Math.max(1,Math.min(Generate.list.size(), userInput.nextInt()));
-			for (int i = 0; i < times; i++)
+			do
 				{
+					int i = (int)(Math.random()*Generate.list.size());
 					int getAnswer = JOptionPane.showOptionDialog(frame, Generate.list.get(i).getConstruct()+" "+Generate.list.get(i).getStem(), "What will the output be?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, option[0]);
 					if (option[getAnswer].equals(Generate.list.get(i).getAnswer()))
 						{
-							System.out.println("You got it.");
+							JOptionPane.showMessageDialog(frame, "You got it.");
 							point++;
 						}
 					else
 						{
-							System.out.println("Wrong!");
+							JOptionPane.showMessageDialog(frame, "Wrong!");
 						}
-				}
-			System.out.println("your point is "+point);
+					Generate.list.remove(i);
+				}while(Generate.list.size()>12-times);
+			JOptionPane.showMessageDialog(frame, "Your point is "+point);
 		}
 	}
